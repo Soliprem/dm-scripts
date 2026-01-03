c5dd2092e127d5a902e38bf935a8792cebef1b52
Soliprem <franci.solidoro@gmail.com>
2026-01-03 11:40:08 +0100

    flake: restructure to make scripts actually run

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
 
@@ -28,43 +34,43 @@
           xorg.xrandr
           light
           upower
-          procps          #  dm-kill
-          
+          procps # dm-kill
+
           # Network
-          networkmanager  # dm-wifi
+          networkmanager # dm-wifi
           curl
-          bind            # dm-ip
-          iproute2        # dm-ip
-          
+          bind # dm-ip
+          iproute2 # dm-ip
+
           # Multimedia
           ffmpeg
           mpv
           yt-dlp
-          maim            # for dm-maim
-          slop            # for dm-maim/dm-record selections
+          maim # for dm-maim
+          slop # for dm-maim/dm-record selections
           mpc
-          mpd             # for dm-music
-          easyeffects     # for dm-eq-profiles
-          
+          mpd # for dm-music
+          easyeffects # for dm-eq-profiles
+
           # Document/Image Viewers
-          zathura         # for dm-documents
-          imv             # for dm-setbg
-          sxiv            # for dm-setbg
-          
+          zathura # for dm-documents
+          imv # for dm-setbg
+          sxiv # for dm-setbg
+
           # Background/Wallpaper
           xwallpaper
           swaybg
           betterlockscreen
-          
+
           # Misc / Script specific
-          jq              # for dm-websearch, dm-ip, dm-pipewire
-          yad             # for dm-weather
-          man-db          # for dm-man
-          udisks2         # for dm-usbmount
+          jq # for dm-websearch, dm-ip, dm-pipewire
+          yad # for dm-weather
+          man-db # for dm-man
+          udisks2 # for dm-usbmount
           translate-shell # for dm-dictionary/dm-translate
-          sqlite          # for dm-bookman
-          bc              
-          
+          sqlite # for dm-bookman
+          bc
+
           didyoumean
         ];
 
@@ -72,38 +78,47 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "dmscripts";
-          version = "git"; 
+          version = "git";
 
           src = ./.;
 
-          nativeBuildInputs = with pkgs; [ makeWrapper pandoc ];
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            pandoc
+          ];
 
           installPhase = ''
             mkdir -p $out/bin $out/share/dmscripts
-
-            # Install all scripts
             cp -r scripts/* $out/bin/
             chmod +x $out/bin/*
-
-            # Install default config
             cp config/config $out/share/dmscripts/config
 
-            # Patch the helper script to point to the nix store config
+            # Patch scripts to use the absolute store path for the helper
+            for script in $out/bin/dm-*; do
+              substituteInPlace "$script" \
+                --replace "source ./_dm-helper.sh" "source $out/bin/_dm-helper.sh" \
+                --replace "source _dm-helper.sh" "source $out/bin/_dm-helper.sh"
+            done
+
+            # Point helper to the nix-provided config
             substituteInPlace $out/bin/_dm-helper.sh \
               --replace "/etc/dmscripts/config" "$out/share/dmscripts/config"
           '';
 
-          # Wrap all scripts with the runtime dependencies in PATH
           postFixup = ''
-            for script in $out/bin/*; do
-              wrapProgram $script \
+            # don't wrap the helper, or it'll hit its guard clause
+            for script in $(find $out/bin -type f -name "dm-*"); do
+              wrapProgram "$script" \
                 --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
             done
           '';
         };
 
         devShells.default = pkgs.mkShell {
-          packages = runtimeDeps ++ [ pkgs.shellcheck pkgs.shfmt ];
+          packages = runtimeDeps ++ [
+            pkgs.shellcheck
+            pkgs.shfmt
+          ];
         };
       }
     );
